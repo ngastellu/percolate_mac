fc9a9b0327619a95a3a25e620737576f4c729456
Nico <nicolas.gastellu@mail.mcgill.ca>
2023-06-08 18:50:10 -0400

    Changed the way `percolate` scans distances; it now pre-sorts and tests only distances found

--- deploy_percolate.py
+++ deploy_percolate.py
@@ -61,6 +61,8 @@ print("Computing AO gammas...")
 agaL, agaR = qcm.AO_gammas(pos,ga)
 print("Computing MO gammas...")
 gamL, gamR = qcm.MO_gammas(M,agaL, agaR, return_diag=True)
+np.save(f'gamL_40x40-{sample_index}.npy',gamL)
+np.save(f'gamR_40x40-{sample_index}.npy',gamR)
 
 
 # ******* 3: Define strongly-coupled MOs *******
@@ -77,14 +79,8 @@ coms = qcm.MO_com(pos, M)
 
 for T in Ts:
     edArr, rdArr = diff_arrs(energies, coms, a0=1, eF=0)
-    distMA = (edArr/2*kB*T)
-
-    # pick initial guess of critical distance
-    d0 = min([np.min(distMA), np.mean(distMA) - 1.5*np.std(distMA)]) # distribution is approx. log-normal is P(mu - 1.5sigma) is already v small
-
-
     # ******* 5: Get spanning cluster *******
-    conduction_clusters, dcrit, A = percolate(energies, pos, M, gamL_tol=gamL_tol,gamR_tol=gamR_tol, dmin=d0, dstep=0.1, return_adjmat=True, distance='logMA',MOgams=(gamL, gamR))
+    conduction_clusters, dcrit, A = percolate(energies, pos, M, gamL_tol=gamL_tol,gamR_tol=gamR_tol, return_adjmat=True, distance='logMA',MOgams=(gamL, gamR), dArrs=(edArr,rdArr))
 
     with open(f'out_percolate-{T}K.pkl', 'wb') as fo:
         pickle.dump((conduction_clusters,dcrit,A), fo)
