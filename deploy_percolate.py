a8c29214f6e0377789b0a720aaee3ff39b75f81d
Nico <nicolas.gastellu@mail.mcgill.ca>
2023-06-08 13:18:05 -0400

    Wrote `diff_arrs` function in `percolate.py` to move time-intensive distance array computations out of `percolate`. This allows for a single evaluation of these distance arrays while allowing T to vary.

--- deploy_percolate.py
+++ deploy_percolate.py
@@ -4,40 +4,51 @@ import sys
 import pickle
 from os import path
 import numpy as np
-from mpi4py import MPI
+#from mpi4py import MPI
 import qcnico.qchemMAC as qcm
 from qcnico.coords_io import read_xsf
-from percolate import dArray_MA, percolate, plot_cluster
+from percolate import diff_arrs, percolate, plot_cluster
 
 
-sample_index = int(sys.argv[1])
+sample_index = 2 #int(sys.argv[1])
 
 
 
 # ******* 0: Partition tasks over different cores *******
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-nprocs = comm.Get_size()
+# comm = MPI.COMM_WORLD
+# rank = comm.Get_rank()
+# nprocs = comm.Get_size()
 
-all_Ts = np.arange(0,1050,50)
-ops_per_rank = all_Ts.shape[0] // nprocs
-if rank == nprocs -1:
-    Ts = all_Ts[rank*ops_per_rank:]
-else:
-    Ts = all_Ts[rank*ops_per_rank:(rank+1)*ops_per_rank]
+# all_Ts = np.arange(0,1050,50)
+# ops_per_rank = all_Ts.shape[0] // nprocs
+# if rank == nprocs -1:
+#     Ts = all_Ts[rank*ops_per_rank:]
+# else:
+#     Ts = all_Ts[rank*ops_per_rank:(rank+1)*ops_per_rank]
 
+Ts = [40,100,300]
+kB = 8.617e-5
 
 # ******* 1: Load data *******
-arpackdir = path.expanduser('~/scratch/ArpackMAC/40x40')
-pos_dir = path.expanduser('~/scratch/clean_bigMAC/40x40/relax/relaxed_structures')
-sample_dir = f'sample-{sample_index}'
+# arpackdir = path.expanduser('~/scratch/ArpackMAC/40x40')
+# pos_dir = path.expanduser('~/scratch/clean_bigMAC/40x40/relax/relaxed_structures')
+# sample_dir = f'sample-{sample_index}'
+
+datadir = path.expanduser('~/Desktop/simulation_outputs/percolation/40x40')
+edir = 'eARPACK'
+Mdir = 'MOs_ARPACK'
+posdir = 'structures'
 
 mo_file = f'MOs_ARPACK_bigMAC-{sample_index}.npy'
 energy_file = f'eARPACK_bigMAC-{sample_index}.npy'
 
-mo_path = path.join(arpackdir,sample_dir,mo_file)
-energy_path = path.join(arpackdir,sample_dir,energy_file)
-pos_path = path.join(pos_dir,sample_dir,'bigMAC-{sample_index}_relaxed.xsf')
+# mo_path = path.join(arpackdir,sample_dir,mo_file)
+# energy_path = path.join(arpackdir,sample_dir,energy_file)
+# pos_path = path.join(pos_dir,sample_dir,'bigMAC-{sample_index}_relaxed.xsf')
+
+mo_path = path.join(datadir,Mdir,mo_file)
+energy_path = path.join(datadir,edir,energy_file)
+pos_path = path.join(datadir,posdir,'bigMAC-{sample_index}_relaxed.xsf')
 
 energies = np.load(energy_path)
 M =  np.load(mo_path)
@@ -65,7 +76,8 @@ biggaR_inds = (gamR > gamR_tol).nonzero()[0]
 coms = qcm.MO_com(pos, M)
 
 for T in Ts:
-    distMA = dArray_MA(energies,coms,T, a0=30)
+    edArr, rdArr = diff_arrs(energies, coms, a0=1, eF=0)
+    distMA = (edArr/2*kB*T)
 
     # pick initial guess of critical distance
     d0 = min([np.min(distMA), np.mean(distMA) - 1.5*np.std(distMA)]) # distribution is approx. log-normal is P(mu - 1.5sigma) is already v small
