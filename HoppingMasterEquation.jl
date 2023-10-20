497c8e0bd82ab88d24dd29ba36b49369bc1f71e5
Nico <nicolas.gastellu@mail.mcgill.ca>
2023-10-20 18:06:27 -0400

    Finished initial implementation of master equation solver using implicit iteration. Getting nonsense results. Still digging.

--- HoppingMasterEquation.jl
+++ HoppingMasterEquation.jl
@@ -2,6 +2,7 @@ module HoppingMasterEquation
 
     using LinearAlgebra
 
+    export run_HME
 
     const kB = 8.617333262e-5 # Boltzmann constant in eV/K
     const e = 1.0 # positron charge
@@ -12,11 +13,114 @@ module HoppingMasterEquation
     end
 
     function initialise_P(energies, pos, T, E)
-        return fermi_dirac(energies - e*np.dot(E,pos), T) # !!! Assuming NEGATIVE charge !!!
+        N = size(energies, 1)
+        Pinit = zeros(N)
+        for i=1:N
+            # println(energies[i] - e*dot(E, pos[i,:]))
+            Pinit[i] = fermi_dirac(energies[i] - e*dot(E, pos[i,:]), T) # !!! Assuming NEGATIVE charge !!!
+        end
+        return Pinit
+    end
+
+    function miller_abrahams_asymm(energies, pos, T, E; α=1.0/30.0)
+        N = size(energies,1)
+        β = 1.0/(kB*T)
+        K = zeros(N,N)
+
+        for i=1:N
+            energies[i] -= e * dot(E,pos[i,:])
+        end
+
+        for i=1:N
+            for j=1:N
+                ΔE = energies[i] - energies[j]
+                ΔR = norm(pos[i,:] - pos[j,:])
+                if ΔE < 0
+                    K[i,j] = exp(-2*α*ΔR)
+                else
+                    K[i,j] = exp(-2*α*ΔR - β*ΔE)
+                end
+
+                if i == j
+                    K[i,j] = 0
+                end
+            end
+        end
+        return K
     end
     
+    function iterate_implicit(Pold, rates)
+        norms = sum(rates,dims=2)
+        N = size(rates,1)
+        Pnew = zeros(N)
+        for i=1:N
+            sum_top = 0
+            sum_bot = 0
+            for j=1:i
+                sum_top += Pnew[j]*rates[j,i]
+                sum_bot += (rates[j,i] - rates[i,j])*Pnew[j]
+            end
+            for j=i+1:N
+                sum_top += Pold[j]*rates[j,i]
+                sum_bot += (rates[j,i] - rates[i,j])*Pold[j]
+            end
+            Pnew[i] = (sum_top/norms[i]) / (1 - sum_bot/norms[i])
+        end
+        return Pnew
+    end
 
+    function solve(Pinit, rates; maxiter=1000000, ϵ=1e-6)
+        N = size(Pinit,1)
+        # println(N)
+        cntr = 1
+        Pnew = Pinit
+        converged = false
+        conv = zeros(maxiter,2)
+        while cntr ≤ maxiter && !converged
+            println(cntr)
+            Pold = Pnew
+            Pnew = iterate_implicit(Pold,rates)
+            ΔP = abs.(Pnew-Pold)
+            converged = all(ΔP .< ϵ)
+            conv[cntr, 1] = maximum(ΔP)
+            conv[cntr,2] = sum(ΔP)/N
+            println(conv[cntr,:])
+            cntr += 1
+        end
+        return Pnew, conv
+    end
 
+    function carrier_velocity(rates, occs, pos)
+        println(size(occs[1,:]))
+        println(size(occs[2,:]))
+        N = size(rates,1)
+        d = size(pos,2)
+        v = zeros(d)
+        for i=1:N
+            for j=1:N
+                ΔR = pos[j,:] - pos[i,:] # !!! Assuming NEGATIVE charge carriers !!!
+                v += rates[i,j]*occs[i]*(1-occs[j])*ΔR
+            end
+        end
+        return v
+    end
 
-
-end 
+    function run_HME(energies, pos, temps, E; maxiter=1000000, ϵ=1e-8)
+        nb_temps = size(temps,1)
+        N = size(energies, 1)
+        d = size(pos, 2)
+        Pfinal = zeros(N)
+        conv = zeros(maxiter)
+        velocities = zeros(nb_temps, d)
+        for k=1:nb_temps
+            T = temps[k]
+            K = miller_abrahams_asymm(energies, pos, T, E)
+            # println(K)
+            P0 = initialise_P(energies, pos, T, E)
+            # println(P0)
+            Pfinal, conv = solve(P0,K; maxiter=maxiter, ϵ=ϵ)
+            velocities[k,:] = carrier_velocity(K, Pfinal, pos)
+        end
+        return velocities, Pfinal, conv
+    end
+end
---
+++ RunHME.jl
@@ -0,0 +1,29 @@
+module RunHME
+
+    include("./HoppingMasterEquation.jl")
+
+    using .HoppingMasterEquation, PyCall
+
+    nsample = 150
+
+    percolate_datadir = "/Users/nico/Desktop/simulation_outputs/percolation/40x40/percolate_output/sample-$(nsample)/"
+
+    py"""
+    import numpy as np
+    centers = np.load($percolate_datadir + 'cc.npy')
+    site_energies = np.load($percolate_datadir + 'ee.npy')
+    """
+
+    centers = PyArray(py"centers"o)
+    site_energies = PyArray(py"site_energies"o)
+
+    E = [1 0]/400
+
+    temps = collect(100:10:400)
+    velocities, occs, conv = run_HME(site_energies, centers, temps, E)
+
+    py"""np.save('velocities.npy', np.vstack(($(PyObject(temps)), np.linalg.norm($(PyObject(velocities)),axis=1))))
+    np.save('final_occs.npy', $(PyObject(occs)))
+    np.save('conv.npy', $(PyObject(conv)))
+    """
+end
