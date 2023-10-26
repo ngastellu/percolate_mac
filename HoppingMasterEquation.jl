c6c72fbb52c7c55657ed424e74ee342feffde2ad
Nico <nicolas.gastellu@mail.mcgill.ca>
2023-10-26 17:50:20 -0400

    Finalised implementation of toy lattice model. Still running into some converging issues. Now entering debugging/diagnostics phase.

--- HoppingMasterEquation.jl
+++ HoppingMasterEquation.jl
@@ -1,23 +1,57 @@
 module HoppingMasterEquation
 
-    using LinearAlgebra
+    using LinearAlgebra, Random
 
-    export run_HME
+    export run_HME, lattice_hopping_model
 
     const kB = 8.617333262e-5 # Boltzmann constant in eV/K
     const e = 1.0 # positron charge
 
     function fermi_dirac(energy,T)
         β = 1.0/(kB*T)
-        return exp(β*energy)
+        return 1.0/( 1 + exp(β*energy))
     end
 
-    function initialise_P(energies, pos, T, E)
+    function initialise_P(energies, pos, L_inds, R_inds, T, E, μL)
         N = size(energies, 1)
+        d = size(pos,2) # spatial dimension
         Pinit = zeros(N)
+        nL = size(L_inds,1)
+        # nR = size(R_inds,1)
+        # mincoord_E = Inf
+        # maxcoord_E = -Inf
+        # minpos = zeros(d)
+        # maxpos = zeros(d)
+        # for i=1:nL
+        #     Ecoord = dot(E,pos[L_inds[i],:])
+        #     if Ecoord < mincoord_E
+        #         mincoord_E = Ecoord
+        #         minpos = pos[L_inds[i],:]
+        #     end
+        # end
+        # for i=1:nR
+        #     Ecoord = dot(E,pos[R_inds[i],:])
+        #     if Ecoord > maxcoord_E
+        #         maxcoord_E = Ecoord
+        #         maxpos = pos[R_inds[i],:]
+        #     end
+        # end
+        # μR = μL - e*dot(E,maxpos-minpos) # Make sure lead voltages are consistent with imposed elecrtic field
+
         for i=1:N
             # println(energies[i] - e*dot(E, pos[i,:]))
-            Pinit[i] = fermi_dirac(energies[i] - e*dot(E, pos[i,:]), T) # !!! Assuming NEGATIVE charge !!!
+            # if i ∉ (L_inds ∪ R_inds)
+            #     Pinit[i] = fermi_dirac(energies[i] - e*dot(E, pos[i,:]), T) # !!! Assuming NEGATIVE charge !!!
+            # elseif i ∈ L_inds
+            if i ∈ L_inds
+                Pinit[i] = fermi_dirac(energies[i]-μL,T)
+            else
+                Pinit[i] = fermi_dirac(energies[i] - e*dot(E, pos[i,:]), T) # !!! Assuming NEGATIVE charge !!!
+            end
+            # else # ⟺ i ∈ R_inds
+            #     Pinit[i] = fermi_dirac(energies[i]-μR,T)
+            # end
+            # Pinit[i] = fermi_dirac(energies[i] - e*dot(E, pos[i,:]), T) # !!! Assuming NEGATIVE charge !!!
         end
         return Pinit
     end
@@ -49,11 +83,16 @@ module HoppingMasterEquation
         return K
     end
     
-    function iterate_implicit(Pold, rates)
+    function iterate_implicit(Pold, rates, L_inds, R_inds)
         norms = sum(rates,dims=2)
         N = size(rates,1)
         Pnew = zeros(N)
         for i=1:N
+            # if i ∈ (L_inds ∪ R_inds) #impose fixed occupations at the boundaries of the system
+            # if i ∈ L_inds #impose fixed occupations at the boundaries of the system
+                # Pnew[i] = Pold[i]
+                # continue
+            # end
             sum_top = 0
             sum_bot = 0
             for j=1:i
@@ -69,21 +108,22 @@ module HoppingMasterEquation
         return Pnew
     end
 
-    function solve(Pinit, rates; maxiter=1000000, ϵ=1e-6)
+    function solve(Pinit, rates, L_inds, R_inds; maxiter=1000000, ϵ=1e-6)
         N = size(Pinit,1)
         # println(N)
         cntr = 1
         Pnew = Pinit
         converged = false
-        conv = zeros(maxiter,2)
+        conv = zeros(maxiter,3)
         while cntr ≤ maxiter && !converged
             println(cntr)
             Pold = Pnew
-            Pnew = iterate_implicit(Pold,rates)
+            Pnew = iterate_implicit(Pold,rates, L_inds, R_inds)
             ΔP = abs.(Pnew-Pold)
             converged = all(ΔP .< ϵ)
             conv[cntr, 1] = maximum(ΔP)
             conv[cntr,2] = sum(ΔP)/N
+            conv[cntr,3] = argmax(ΔP)
             println(conv[cntr,:])
             cntr += 1
         end
@@ -105,22 +145,59 @@ module HoppingMasterEquation
         return v
     end
 
-    function run_HME(energies, pos, temps, E; maxiter=1000000, ϵ=1e-8)
+    function run_HME(energies, pos, L_inds, R_inds, temps, E, μL; maxiter=1000000, ϵ=1e-8)
         nb_temps = size(temps,1)
         N = size(energies, 1)
         d = size(pos, 2)
-        Pfinal = zeros(N)
+        Pfinal = zeros(nb_temps,N)
+        Pinit = zeros(nb_temps,N)
         conv = zeros(maxiter)
         velocities = zeros(nb_temps, d)
         for k=1:nb_temps
             T = temps[k]
+            println("\n\n ************ T = $T *************")
             K = miller_abrahams_asymm(energies, pos, T, E)
             # println(K)
-            P0 = initialise_P(energies, pos, T, E)
+            P0 = initialise_P(energies, pos, L_inds, R_inds, T, E, μL)
+            Pinit[k,:] = P0
             # println(P0)
-            Pfinal, conv = solve(P0,K; maxiter=maxiter, ϵ=ϵ)
+            Pfinal[k,:], conv = solve(P0, K, L_inds, R_inds; maxiter=maxiter, ϵ=ϵ)
             velocities[k,:] = carrier_velocity(K, Pfinal, pos)
         end
-        return velocities, Pfinal, conv
+        return velocities, Pfinal, Pinit, conv
     end
+
+
+    function lattice_hopping_model(pos, L_inds, R_inds, temps, E, μL, ntrials)
+
+        N = size(pos,1)
+        ntemps = size(temps,1)
+        occs_init = zeros(ntemps, N)
+        occs_final = zeros(ntemps, N)
+        energies_saved = zeros(N)
+        velocities_norm = zeros(ntrials, ntemps)
+
+        for i=1:ntrials
+            energies = randn(N)
+            # energies = rand(N) * (2 * E0) .- E0
+    
+            velocities, occs, occs0, conv = run_HME(energies, pos, L_inds, R_inds, temps, E, μL;ϵ=1e-7)
+    
+            for j=1:ntemps
+                velocities_norm[i,j] = norm(velocities[j,:])
+            end
+    
+            if i == ntrials #save only occupations of the last disorder realisation (averaging over different realisations doesn't really make sense)
+                occs_init = occs0
+                occs_final = occs
+                energies_saved = energies
+            end
+        end
+    
+        velocities_avg = sum(velocities_norm,dims=1)/ntrials
+
+        return velocities_avg, occs_init, occs_final, energies_saved
+
+    end
+
 end
--- TestHME.jl
+++ TestHME.jl
@@ -1,36 +1,49 @@
 module TestHME
     include("./HoppingMasterEquation.jl")
 
-    using .HoppingMasterEquation, Random, PyCall
+    using .HoppingMasterEquation, Random, PyCall, LinearAlgebra
+
+    Random.seed!(64)
 
 
-    N = 50
+    n = 50
     E0 = 1.0
+    ntrials = 100
 
-    pos = zeros(N,N,2)
+    pos = zeros(n,n,2)
 
-    for i=1:N
-        for j=1:N
-            pos[i,j,:] = [i,j]
+    for i=1:n
+        for j=1:n
+            pos[i,j,:] = [i-1,j-1]
         end
     end
 
-    pos = reshape(pos, N*N, 2)
+    pos = reshape(pos, n*n, 2)
+
+    L_inds = findall(pos[:,1] .== 0)
+    R_inds = findall(pos[:,1] .== n-1)
 
     println(typeof(pos))
 
-    energies = rand(N*N) * (2 * E0) .- E0
-
-    E = [1,0] / (N-1)
+    E = [1,0] / (n-1)
+    μL = 0.0
  
     temps = collect(100:10:400)
-    velocities, occs, conv = run_HME(energies, pos, temps, E)
+    # temps = collect(100:10:120)
+
+    vs, occs0, occs, eners = lattice_hopping_model(pos, L_inds, R_inds, temps, E, μL,ntrials)
+
+    
 
     py"""import numpy as np 
-    np.save('velocities_test.npy', np.vstack(($(PyObject(temps)), np.linalg.norm($(PyObject(velocities)),axis=1))))
-    np.save('final_occs_test.npy', $(PyObject(occs)))
-    np.save('conv_test.npy', $(PyObject(conv)))
     np.save('pos_test.npy', $(PyObject(pos)))
+    np.save('velocities_test.npy', np.vstack(($(PyObject(temps)), np.linaVlg.norm($(PyObject(vs)),axis=1))))
+    # np.save('temps_test.npy', $(PyObject(temps)))
+    # np.save('velocities_test.npy', $(PyObject(vs)))
+    np.save('final_occs_test.npy', $(PyObject(occs)))
+    np.save('initial_occs_test.npy', $(PyObject(occs0)))
+    np.save('energies_test.npy', $(PyObject(eners)))
+    # np.save('conv_test.npy', $(PyObject(conv)))
     """
 
 end
