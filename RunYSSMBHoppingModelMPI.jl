47640c403e64dfe8e68ee39dbccd03901116b660
Nico <nicolas.gastellu@mail.mcgill.ca>
2023-10-31 17:59:05 -0400

    Fixed typo in `RunYSSMBHoppingModel.jl`.

--- RunYSSMBHoppingModelMPI.jl
+++ RunYSSMBHoppingModelMPI.jl
@@ -22,7 +22,7 @@ temps = collect(100:10:400)
 ν = 0.007  # picked this stdev to roughly match the Gaussian DOS in the paper (DOI: 10.1103/PhysRevB.63.085202)
 
 
-energies, velocities, Pinits, Pfinals = YSALMB_lattice_model(temps,density; νeff = ν)
+energies, velocities, Pinits, Pfinals = YSSMB_lattice_model(temps,density; νeff = ν)
 
 py"""import numpy as np
 np.save('temps.npy', $(PyObject(temps)))
