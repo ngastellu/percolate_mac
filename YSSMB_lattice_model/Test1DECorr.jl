2007fbe7ebb3c2fc7487323d2a5d6ed25ac675b1
Nico <nicolas.gastellu@mail.mcgill.ca>
2023-12-05 18:45:42 -0500

    `Test1DECorr.jl` only runs on `N=1024` sites now (removed outer loop).

--- YSSMB_lattice_model/Test1DECorr.jl
+++ YSSMB_lattice_model/Test1DECorr.jl
@@ -12,27 +12,25 @@ T = 300
 K = 0.0034
 ν = 0.3
 
-for N ∈ [32, 64, 128, 256, 512]
-    println("********** $N **********")
-    for i=1:nsamples
-        println(i)
-        energies, Φ = generate_correlated_esites_1d(a, N, T, K, ν)
-        # energies = real(energies)
+N = 1024
+for i=1:nsamples
+    println(i)
+    energies, Φ = generate_correlated_esites_1d(a, N, T, K, ν)
+    # energies = real(energies)
 
-        py"""import numpy as np
-        import os 
-        nn = $i
-        n = $N
-        ecorr_dir = f'corr_energies_1d_hsymm_{n}/'
-        mgfft_dir = f'ft_mol_geom_field_1d_hsymm_{n}/'
-        if not os.path.exists(ecorr_dir):
-            os.mkdir(ecorr_dir)
-        if not os.path.exists():
-            os.mkdir(mgfft_dir) 
-        np.save(ecorr_dir + f'correlated_energies-{nn}.npy', $(PyObject(energies)))
-        np.save(mgfft_dir + f'mgf_ft-{nn}.npy', $(PyObject(Φ)))
-        """
-    end
+    py"""import numpy as np
+    import os 
+    nn = $i
+    n = $N
+    ecorr_dir = f'corr_energies_1d_complex_fftshift_{n}/'
+    mgfft_dir = f'ft_mol_geom_field_1d_complex_fftshift_{n}/'
+    if not os.path.exists(ecorr_dir):
+        os.mkdir(ecorr_dir)
+    if not os.path.exists(mgfft_dir):
+        os.mkdir(mgfft_dir) 
+    np.save(ecorr_dir + f'correlated_energies-{nn}.npy', $(PyObject(energies)))
+    np.save(mgfft_dir + f'mgf_ft-{nn}.npy', $(PyObject(Φ)))
+    """
 end
 
 end
