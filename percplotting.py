d92521fd5f9e3890f062f28e46aadef59d487441
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-03-06 22:36:04 -0500

    Started gathering useful plotting functions in `percplotting.py`.

---
+++ percplotting.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt 
+from qcnico import plt_utils
+
+def dcrit_hists(dcrits,temps,nbins,plot_inds=None,colormap='coolwarm',alpha=0.6,usetex=True,plt_objs=None,show=True):
+    Tcm = plt_utils.get_cm(temps,colormap,max_val=1.0)
+    
+    # If indices aren't specified, plot all the histograms
+    # We still pass all of the dcrits to this function (even those we don't plot) to get a better
+    # contrasted colormap.
+    if plot_inds is None:
+        plot_inds = range(len(temps))
+
+
+    if usetex:
+        plt_utils.setup_tex()
+    
+    if plt_objs is None:
+        fig, ax = plt.subplots()
+    else:
+        fig, ax = plt_objs
+
+    for k in plot_inds:
+        plt_utils.histogram(dcrits[:,k],nbins=nbins,show=False, normalised=False, plt_objs=(fig,ax),
+            plt_kwargs={'alpha': alpha, 'color': Tcm[k], 'label': f'$T = {temps[k]}$K'})
+    
+    ax.set_xlabel('Critical distance $d_{c}$')
+    plt.legend()
+    if show:
+        plt.show()
+    else:
+        return fig, ax
--- test_rough_integration.py
+++ test_rough_integration.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from qcnico.plt_utils import setup_tex
 from utils_analperc import get_dcrits, rough_integral_sigma, saddle_pt_sigma
+from percplotting import dcrit_hists
 
 
 temps = np.arange(40,440,10)
@@ -22,11 +23,15 @@ sigma_saddle = saddle_pt_sigma(dcrits, nbins=30)
 setup_tex()
 fig, ax = plt.subplots()
 
-ax.plot(1000/temps,np.log(sigma_rough),'ro-',label='rough')
-ax.plot(1000/temps,np.log(sigma_saddle),'bo-',label='saddle')
+ax.plot(1000/temps,np.log(sigma_rough),'ro-',label='new')
+ax.plot(1000/temps,np.log(sigma_saddle),'bo-',label='old')
+ax.set_xlabel('$1000/T$')
+ax.set_ylabel('$\log\sigma$')
+ax.set_title('Testing new method of estimating $\sigma(T)$')
 plt.legend()
 plt.show()
 
+dcrit_hists(dcrits, temps, 30, plot_inds=[0,10,30,39])
 
 
 
