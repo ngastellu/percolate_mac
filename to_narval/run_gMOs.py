ae2339b9d6a10973112bc2c640475741a635a062
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-04-25 20:02:15 -0400

    changed name of parent module in `run_gMOS.py` to `rollback_percolate_mac` for proper running on Narval

--- to_narval/run_gMOs.py
+++ to_narval/run_gMOs.py
@@ -3,15 +3,19 @@
 import sys
 from os import path
 import numpy as np
-from percolate_mac.deploy_percolate import load_data, run_gridMOs
+import qcnico.qchemMAC as qcm
+from qcnico.coords_io import read_xsf
+from qcnico.remove_dangling_carbons import remove_dangling_carbons
+from rollback_percolate_mac.deploy_percolate import run_gridMOs, load_data
+
 
 
 n = int(sys.argv[1])
-struc_type = sys.argv[2]
-mo_type = sys.argv[3]
+structype='tempdot6'
+motype='virtual'
 
 temps = np.arange(40,440,10)
 dV = 0.0
 
-pos, e, M, gamL, gamR = load_data(n, struc_type, mo_type, compute_gammas=True)
-run_gridMOs(pos,e,M,gamL,gamR,temps, dV, eF=0)
+pos, e, M, gamL, gamR = load_data(n, structype, motype,  compute_gammas=False)
+run_gridMOs(pos,e,M,gamL,gamR,temps, dV, eF=0, compute_centres=False)
