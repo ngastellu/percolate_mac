e2462728909c1829c8b533843ff1e77ae3df6596
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-05-15 17:44:10 -0400

    Modified deployment of varible-a percolation to be compatible with `rollback` branch

--- to_narval/run_gMOs_var_a.py
+++ to_narval/run_gMOs_var_a.py
@@ -3,8 +3,8 @@
 import sys
 from os import path
 import numpy as np
-from var_a_percolate_mac.deploy_percolate import load_data, load_var_a_data, run_percolate
-from var_a_percolate_mac.MOs2sites import LR_MOs
+from var_a_percolate_mac.deploy_percolate import load_data
+from rollback_percolate_mac import run_var_a
 
 
 n = int(sys.argv[1])
@@ -15,6 +15,4 @@ temps = np.arange(40,440,10)
 dV = 0.0
 
 pos, e, M, gamL, gamR = load_data(n, struc_type, mo_type, compute_gammas=False)
-L, R = LR_MOs(gamL, gamR)
-sites_pos, sites_energies, sites_radii, ii = load_var_a_data(datadir='.')
-run_percolate(sites_pos,sites_energies,L,R,temps, dV, a0=sites_radii, eF=0, jitted=False)
+run_var_a(pos,M,gamL,gamR,temps, dV, eF=0)
