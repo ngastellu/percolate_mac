Fixed zero length bug for flagged radii and moved density mask computation outside of label loop, in `site_radii`

MOs2sites.py
@@ -434,12 +434,13 @@ def site_radii(pos, M, n, labels, hyperlocal='sites', density_threshold=0, flagg
         flagged_radii = np.zeros(len(flagged_labels))
         m = 0
 
+    if density_threshold > 0:
+        density = np.abs(M[:,n])**2
+        density_mask = (density > density_threshold) 
+
     for k, l in enumerate(unique_labels):
         mask = (labels==l)  
         if density_threshold > 0:
-            density = np.abs(M[:,n])**2
-            density_mask = (density > density_threshold) 
-            # print(f'[site_radii] Ignoring {N - density_mask.sum()} atoms out of {N} total.')
             mask *= density_mask
         # print(f'# of atoms in cluster {l}: ', mask.sum())
         # masked_M = np.copy(M)
@@ -464,7 +465,7 @@ def site_radii(pos, M, n, labels, hyperlocal='sites', density_threshold=0, flagg
             if radii[k] > max_r: 
                 radii[k] = 0
 
-    if flagged_labels is not None:
+    if flagged_labels is not None and len(flagged_radii) > 0:
         print(f'MAX flagged radius = {np.max(flagged_radii)} ; MEAN flagged radius = {np.mean(flagged_radii)}')
         
     # filter nans induced be wavefunction re-normalisation

test_full_site_radii_generation.py
@@ -3,13 +3,14 @@
 from time import perf_counter
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 from scipy.spatial import KDTree
 from qcnico import plt_utils
 from qcnico.qchemMAC import AO_gammas, MO_gammas
 from qcnico.coords_io import read_xsf
 from qcnico.remove_dangling_carbons import remove_dangling_carbons
-from qcnico.qcplots import plot_MO
-from MOs2sites import generate_sites_radii_list, LR_MOs, sites_mass
+from qcnico.qcplots import plot_MO, plot_atoms
+from MOs2sites import generate_sites_radii_list, LR_MOs, sites_mass, assign_AOs, get_MO_loc_centers_opt, site_radii
 
 
 
@@ -32,8 +33,8 @@ tree = KDTree(pos)
 # agL, agR = AO_gammas(pos,gamma,brute_force=True)
 # gamL, gamR = MO_gammas(M, agL, agR, return_diag=True) 
 
-gamL = np.load('/Users/nico/Desktop/simulation_outputs/percolation/40x40/percolate_output/zero_field/virt_100x100_gridMOs_var_a/sample-42/gamL_40x40-42_virtual.npy')
-gamR = np.load('/Users/nico/Desktop/simulation_outputs/percolation/40x40/percolate_output/zero_field/virt_100x100_gridMOs_var_a/sample-42/gamR_40x40-42_virtual.npy')
+gamL = np.load(f'/Users/nico/Desktop/simulation_outputs/percolation/40x40/percolate_output/zero_field/virt_100x100_gridMOs_var_a/sample-{nsample}/gamL_40x40-{nsample}_virtual.npy')
+gamR = np.load(f'/Users/nico/Desktop/simulation_outputs/percolation/40x40/percolate_output/zero_field/virt_100x100_gridMOs_var_a/sample-{nsample}/gamR_40x40-{nsample}_virtual.npy')
 
 L, R = LR_MOs(gamL, gamR)
 
@@ -44,7 +45,11 @@ max_r = 50.0
 print('Generating sites and radii now...')
 start = perf_counter()
 centers, radii, ee, ii = generate_sites_radii_list(pos, M, L, R, energies, radii_rho_threshold=eps_rho,flag_empty_clusters=flag_empty_clusters,max_r=max_r)
-centers_hl, radii_hl, ee_hl, ii_hl = generate_sites_radii_list(pos, M, L, R, energies,hyperlocal='all', radii_rho_threshold=eps_rho,flag_empty_clusters=flag_empty_clusters,max_r=max_r)
+np.save('ok_centers.npy', centers)
+np.save('ok_radii.npy', radii)
+np.save('ok_ii.npy', ii)
+np.save('ok_ee.npy', ee)
+# centers_hl, radii_hl, ee_hl, ii_hl = generate_sites_radii_list(pos, M, L, R, energies,hyperlocal='all', radii_rho_threshold=eps_rho,flag_empty_clusters=flag_empty_clusters,max_r=max_r)
 end = perf_counter()
 print(f'Done! [{end-start} seconds]')
 
@@ -52,50 +57,92 @@ print('Shape of centers: ', centers.shape)
 print('Shape of ii: ', ii.shape)
 
 masses = np.zeros(radii.shape[0])
+# masses_hl = np.zeros(radii_hl.shape[0])
 r_sorted = np.zeros(radii.shape[0])
+# r_sorted_hl = np.zeros(radii_hl.shape[0])
 k = 0
+l = 0
 for n in range(M.shape[1]):
+    psi = M[:,n]
+
     jj = (ii == n)
     nsites = jj.sum()
     cc = centers[jj,:]
     rr = radii[jj]
-    psi = M[:,n]
     r_sorted[k:k+nsites] = rr
     masses[k:k+nsites] = sites_mass(psi,tree,cc,rr)
     k += nsites
 
+    # jj = (ii_hl == n)
+    # nsites = jj.sum()
+    # cc_hl = centers_hl[jj,:]
+    # rr = radii_hl[jj]
+    # r_sorted_hl[l:l+nsites] = rr
+    # masses_hl[l:l+nsites] = sites_mass(psi,tree,cc_hl,rr)
+    # l += nsites
+
 plt_utils.setup_tex()
 
-plt_utils.multiple_histograms((radii,radii_hl),nbins=200,xlabel='Site radii [\AA]',labels=('$|\psi_n|^2$','$|\psi_n|^4$'))
+# plt_utils.multiple_histograms((radii,radii_hl),nbins=200,xlabel='Site radii [\AA]',labels=('$|\psi_n|^2$','$|\psi_n|^4$'))
 
 fig, ax = plt.subplots()
-ax.scatter(r_sorted,masses,c='r',s=1.0)
+ax.scatter(r_sorted,masses,c='r',s=5.0,label='$|\psi_n|^2$',alpha=0.5)
+# ax.scatter(r_sorted_hl,masses_hl,c='b',s=5.0,label='$|\psi_n|^4$',alpha=0.5)
 ax.set_xlabel('Site radius [\AA]')
 ax.set_ylabel('Site mass')
+plt.legend()
 plt.show()
 
 
 imax_r = np.argmax(radii)
 nn = ii[imax_r]
+print(nn)
 
 rel_centers =  centers[ii == nn]
 rel_radii = radii[ii == nn]
 
-print('REL RADII = ', rel_radii)
-
 fig, ax = plt.subplots()
 plot_MO(pos,M, nn, dotsize=1.0, plt_objs=(fig, ax), loc_centers=rel_centers, loc_radii=rel_radii)
+
 # fig, ax = plt.subplots()
 # plot_MO(pos, M, nn, dotsize=1.0, plt_objs=(fig, ax),show_rgyr=True)
 
+cc, *_ = get_MO_loc_centers_opt(pos, M, nn, 100, threshold_ratio=0.30)
+plot_MO(pos,M, nn, dotsize=1.0,loc_centers=cc)
+print('A priori centers = ', cc)
+psi = M[:,nn]
+clust_cc, ll, flagged_ll = assign_AOs(pos, cc, psi, psi_pow=4, flag_empty_clusters=True)
+print('Clusters centers = ', clust_cc)
+print(ll)
+print(flagged_ll)
+
+fs, fr = site_radii(pos,M,nn,ll,hyperlocal='sites',density_threshold=eps_rho,flagged_labels=flagged_ll,max_r=max_r)
+print('Final sites = ', fs)
+print('Final radii = ', fr)
+
+
+plot_MO(pos,M, nn, dotsize=1.0, loc_centers=fs, loc_radii=fr)
+
+masses = sites_mass(psi,tree,fs,fr)
+print('Masses = ', masses)
+
+cyc = rcParams['axes.prop_cycle'] #default plot colours are stored in this `cycler` type object
+clrs = [d['color'] for d in list(cyc[0:len(clust_cc)])]
+atom_clrs = [clrs[k] for k in ll]
+
+fig,ax = plt.subplots()
+fig, ax = plot_atoms(pos,dotsize=0.1,show=False,plt_objs=(fig,ax),usetex=False,colour=atom_clrs,zorder=1)
+ax.scatter(*clust_cc.T, c=clrs, marker='^', s=5.0)
+ax.scatter(*rel_centers.T, c='r',marker='h', s=5.0)
+plt.show()
 
 # np.random.seed(64)
 # for n in np.random.randint(M.shape[1], size=10):
 #     jj = (ii == n)
-#     print(jj)
+#     # print(jj)
 #     nsites = jj.sum()
 #     cc = centers[jj,:]
-#     print(cc)
+#     # print(cc)
 #     rr = radii[jj]
-#     print(rr)
+#     # print(rr)
 #     plot_MO(pos,M, n, dotsize=1.0, loc_centers=cc, loc_radii=rr,c_rel_size=2)