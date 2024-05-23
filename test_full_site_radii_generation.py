a7089f4e866080d8fcc9b50ed0334f02a4617e54
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-05-23 09:39:58 -0400

    Copied from `assign_AOs`, `site_radii`, and `generate_site_radii_list` from GitHub `49e2ad5`

--- test_full_site_radii_generation.py
+++ test_full_site_radii_generation.py
@@ -3,18 +3,17 @@
 from time import perf_counter
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
 from scipy.spatial import KDTree
 from qcnico import plt_utils
 from qcnico.qchemMAC import AO_gammas, MO_gammas
 from qcnico.coords_io import read_xsf
 from qcnico.remove_dangling_carbons import remove_dangling_carbons
-from qcnico.qcplots import plot_MO, plot_atoms
-from MOs2sites import generate_sites_radii_list, LR_MOs, sites_mass, assign_AOs, get_MO_loc_centers_opt, site_radii
+from qcnico.qcplots import plot_MO
+from MOs2sites import generate_sites_radii_list, LR_MOs, sites_mass
 
 
 
-nsample = 42
+nsample = 64
 gamma = 0.1
 
 # percdir = f'/Users/nico/Desktop/simulation_outputs/percolation/Ata_structures/tempdot5/percolate_output/zero_field/virt_100x100_gridMOs/sample-{nsample}/'
@@ -33,8 +32,8 @@ tree = KDTree(pos)
 # agL, agR = AO_gammas(pos,gamma,brute_force=True)
 # gamL, gamR = MO_gammas(M, agL, agR, return_diag=True) 
 
-gamL = np.load(f'/Users/nico/Desktop/simulation_outputs/percolation/40x40/percolate_output/zero_field/virt_100x100_gridMOs_var_a/sample-{nsample}/gamL_40x40-{nsample}_virtual.npy')
-gamR = np.load(f'/Users/nico/Desktop/simulation_outputs/percolation/40x40/percolate_output/zero_field/virt_100x100_gridMOs_var_a/sample-{nsample}/gamR_40x40-{nsample}_virtual.npy')
+gamL = np.load('/Users/nico/Desktop/simulation_outputs/percolation/40x40/percolate_output/zero_field/virt_100x100_gridMOs_var_a/sample-42/gamL_40x40-42_virtual.npy')
+gamR = np.load('/Users/nico/Desktop/simulation_outputs/percolation/40x40/percolate_output/zero_field/virt_100x100_gridMOs_var_a/sample-42/gamR_40x40-42_virtual.npy')
 
 L, R = LR_MOs(gamL, gamR)
 
@@ -50,6 +49,8 @@ np.save('ok_radii.npy', radii)
 np.save('ok_ii.npy', ii)
 np.save('ok_ee.npy', ee)
 # centers_hl, radii_hl, ee_hl, ii_hl = generate_sites_radii_list(pos, M, L, R, energies,hyperlocal='all', radii_rho_threshold=eps_rho,flag_empty_clusters=flag_empty_clusters,max_r=max_r)
+# centers, radii, ee, ii = generate_sites_radii_list(pos, M, L, R, energies, radii_rho_threshold=eps_rho,flag_empty_clusters=flag_empty_clusters,max_r=max_r)
+centers_hl, radii_hl, ee_hl, ii_hl = generate_sites_radii_list(pos, M, L, R, energies,hyperlocal='all', radii_rho_threshold=eps_rho,flag_empty_clusters=flag_empty_clusters,max_r=max_r)
 end = perf_counter()
 print(f'Done! [{end-start} seconds]')
 
@@ -57,37 +58,39 @@ print('Shape of centers: ', centers.shape)
 print('Shape of ii: ', ii.shape)
 
 masses = np.zeros(radii.shape[0])
-# masses_hl = np.zeros(radii_hl.shape[0])
 r_sorted = np.zeros(radii.shape[0])
-# r_sorted_hl = np.zeros(radii_hl.shape[0])
 k = 0
-l = 0
-for n in range(M.shape[1]):
-    psi = M[:,n]
 
+masses_hl = np.zeros(radii.shape[0])
+r_sorted_hl = np.zeros(radii.shape[0])
+m = 0
+
+for n in range(M.shape[1]):
     jj = (ii == n)
     nsites = jj.sum()
     cc = centers[jj,:]
     rr = radii[jj]
+    psi = M[:,n]
     r_sorted[k:k+nsites] = rr
     masses[k:k+nsites] = sites_mass(psi,tree,cc,rr)
     k += nsites
 
-    # jj = (ii_hl == n)
-    # nsites = jj.sum()
-    # cc_hl = centers_hl[jj,:]
-    # rr = radii_hl[jj]
-    # r_sorted_hl[l:l+nsites] = rr
-    # masses_hl[l:l+nsites] = sites_mass(psi,tree,cc_hl,rr)
-    # l += nsites
-
+    jj = (ii_hl == n)
+    nsites = jj.sum()
+    cc = centers_hl[jj,:]
+    rr = radii_hl[jj]
+    psi = M[:,n]
+    r_sorted_hl[m:m+nsites] = rr
+    masses_hl[m:m+nsites] = sites_mass(psi,tree,cc,rr)
+    m += nsites
+    
 plt_utils.setup_tex()
 
-# plt_utils.multiple_histograms((radii,radii_hl),nbins=200,xlabel='Site radii [\AA]',labels=('$|\psi_n|^2$','$|\psi_n|^4$'))
+plt_utils.multiple_histograms((radii,radii_hl),nbins=200,xlabel='Site radii [\AA]',labels=('$|\psi_n|^2$','$|\psi_n|^4$'))
 
 fig, ax = plt.subplots()
-ax.scatter(r_sorted,masses,c='r',s=5.0,label='$|\psi_n|^2$',alpha=0.5)
-# ax.scatter(r_sorted_hl,masses_hl,c='b',s=5.0,label='$|\psi_n|^4$',alpha=0.5)
+ax.scatter(r_sorted,masses,c='r',s=4.0,alpha=0.6,label='$|\psi_n|^2$')
+ax.scatter(r_sorted_hl,masses_hl,c='b',s=4.0,alpha=0.6,label='$|\psi_n|^4$')
 ax.set_xlabel('Site radius [\AA]')
 ax.set_ylabel('Site mass')
 plt.legend()
@@ -96,16 +99,16 @@ plt.show()
 
 imax_r = np.argmax(radii)
 nn = ii[imax_r]
-print(nn)
 
 rel_centers =  centers[ii == nn]
 rel_radii = radii[ii == nn]
 
+print('REL RADII = ', rel_radii)
+
 fig, ax = plt.subplots()
 plot_MO(pos,M, nn, dotsize=1.0, plt_objs=(fig, ax), loc_centers=rel_centers, loc_radii=rel_radii)
-
-# fig, ax = plt.subplots()
-# plot_MO(pos, M, nn, dotsize=1.0, plt_objs=(fig, ax),show_rgyr=True)
+fig, ax = plt.subplots()
+plot_MO(pos, M, nn, dotsize=1.0, plt_objs=(fig, ax),show_rgyr=True)
 
 # cc, *_ = get_MO_loc_centers_opt(pos, M, nn, 100, threshold_ratio=0.30)
 # plot_MO(pos,M, nn, dotsize=1.0,loc_centers=cc)
@@ -136,13 +139,13 @@ plot_MO(pos,M, nn, dotsize=1.0, plt_objs=(fig, ax), loc_centers=rel_centers, loc
 # ax.scatter(*rel_centers.T, c='r',marker='h', s=5.0)
 # plt.show()
 
-# # np.random.seed(64)
-# # for n in np.random.randint(M.shape[1], size=10):
-# #     jj = (ii == n)
-# #     # print(jj)
-# #     nsites = jj.sum()
-# #     cc = centers[jj,:]
-# #     # print(cc)
-# #     rr = radii[jj]
-# #     # print(rr)
-# #     plot_MO(pos,M, n, dotsize=1.0, loc_centers=cc, loc_radii=rr,c_rel_size=2)
+np.random.seed(64)
+for n in np.random.randint(M.shape[1], size=10):
+    jj = (ii_hl == n)
+    print(jj)
+    nsites = jj.sum()
+    cc = centers_hl[jj,:]
+    print(cc)
+    rr = radii_hl[jj]
+    print(rr)
+    plot_MO(pos,M, n, dotsize=1.0, loc_centers=cc, loc_radii=rr,c_rel_size=2)
