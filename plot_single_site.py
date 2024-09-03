a6d250d3a5ec2c1c9d1d70f6900e93c276785150
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-09-03 16:04:33 -0400

    Wrote script to plot a single hopping. For quick and easy inspection.

---
+++ plot_single_site.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import numpy as np
+from qcnico.plt_utils import setup_tex
+from qcnico.qcplots import plot_MO
+from qcnico.coords_io import read_xyz
+import os
+import sys
+
+
+
+
+structype = 'tempdot5'
+
+if structype == 'tempdot5':
+    off_structype = 'sAMC-300'
+elif structype == 'tempdot6':
+    off_structype = 'sAMC-q400'
+elif structype == '40x40':
+    off_structype = 'sAMC-500'
+else:
+    print(f'Structure type {structype} is invalid! Exiting with error.')
+    sys.exit()
+
+istruc = 39
+istate = 300
+
+
+
+simdir = os.path.expanduser('~/Desktop/simulation_outputs')
+sitesdir = os.path.join(simdir, f'percolation/{structype}/var_radii_data/to_local_sites_data_0.00105_psi_pow1/sample-{istruc}')
+
+posdir = os.path.join(simdir, f'MAC_structures/relaxed_no_dangle/{off_structype}')
+pos  = read_xyz(os.path.join(posdir, f'{('').join(off_structype.split('-'))}-{istruc}.xyz'))
+
+
+S = np.load(os.path.join(sitesdir, 'site_state_matrix.npy'))
+radii = np.load(os.path.join(sitesdir, 'radii.npy')) 
+centers = np.load(os.path.join(sitesdir, 'centers.npy')) 
+ii = np.load(os.path.join(sitesdir, 'ii.npy'))
+
+MO_index = ii[istate]
+print(f'MO index of site {istate} =  {MO_index}')
+print(f'Other sites from the MO # {MO_index}:')
+for nn in (ii == MO_index).nonzero()[0]:
+    print(nn)
+
+plot_MO(pos, S, istate, dotsize=0.5,loc_centers=np.array([centers[istate]]), loc_radii=[radii[istate]])
