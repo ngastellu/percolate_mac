fd448fd7a9a0c4e8db29fe94cc8019a7d06af4ef
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-09-19 13:30:13 -0400

    Changed `get_var_a_data.py` to obtain sites with a lower threshold ratio and minimum distnce during the pre-clustering local maximum search, and to only consider `eps_rho=0.00105`

--- to_narval/get_var_a_data.py
+++ to_narval/get_var_a_data.py
@@ -25,17 +25,18 @@ tree = KDTree(pos)
 L, R = LR_MOs(gamL, gamR)
 
 
-eps_rhos = [0, 1.05e-3, 2e-3, 3e-3]
+eps_rho = 1.05e-3
 flag_empty_clusters = True
 max_r = 50.0
+min_dist = 4.0
+threshold_ratio_peaks = 0.05
 
-for eps_rho in eps_rhos:
-    print(f'_______________ eps_rho = {eps_rho} _______________')
-    print('Generating sites and radii now...')
-    start = perf_counter()
-    centers, radii, ee, ii,labels, site_matrix = generate_sites_radii_list(pos, M, L, R, energies, radii_rho_threshold=eps_rho,flag_empty_clusters=flag_empty_clusters,max_r=max_r,return_labelled_atoms=True,return_site_matrix=True, amplitude_pow=psipow)
-    end = perf_counter()
-    print(f'Done! [{end-start} seconds]')
+print(f'_______________ eps_rho = {eps_rho} _______________')
+print('Generating sites and radii now...')
+start = perf_counter()
+centers, radii, ee, ii,labels, site_matrix = generate_sites_radii_list(pos, M, L, R, energies, radii_rho_threshold=eps_rho,flag_empty_clusters=flag_empty_clusters,max_r=max_r,return_labelled_atoms=True,return_site_matrix=True, amplitude_pow=psipow,min_distance=min_dist,threshold_ratio=threshold_ratio_peaks)
+end = perf_counter()
+print(f'Done! [{end-start} seconds]')
 
 #
 #    print('Generating HYPERLOCAL sites and radii now...')
@@ -81,24 +82,24 @@ for eps_rho in eps_rhos:
 #        masses_hl[m:m+nsites] = sites_mass(psi,tree,cc,rr)
 #        site_iprs_hl[m:m+nsites] = sites_ipr(psi,labels_hl[n],eps_rho=eps_rho)
 #        m += nsites
-        #print(f'new m = {m}\n')
+    #print(f'new m = {m}\n')
 
 
-    npydir = f'sites_data_{eps_rho}_psi_pow{psipow}/' 
-    if not os.path.isdir(npydir):
-        os.mkdir(npydir)
+npydir = f'sites_data_{eps_rho}_psi_pow{psipow}_many_peaks/' 
+if not os.path.isdir(npydir):
+    os.mkdir(npydir)
 
-    np.save(npydir + 'ee.npy', ee)
-    np.save(npydir + 'radii.npy', radii)
-    np.save(npydir + 'centers.npy', centers)
-    np.save(npydir + 'ii.npy', ii)
+np.save(npydir + 'ee.npy', ee)
+np.save(npydir + 'radii.npy', radii)
+np.save(npydir + 'centers.npy', centers)
+np.save(npydir + 'ii.npy', ii)
 #    np.save(npydir + 'rr_v_masses_v_iprs_v_ee.npy',np.vstack((r_sorted,masses,site_iprs,e_sorted)))
-    np.save(npydir + 'site_state_matrix.npy',site_matrix)
+np.save(npydir + 'site_state_matrix.npy',site_matrix)
 
-    
-    #np.save(npydir + 'ee_hl.npy', ee_hl)
-    #np.save(npydir + 'radii_hl.npy', radii_hl)
-    #np.save(npydir + 'centers_hl.npy', centers_hl)
-    #np.save(npydir + 'ii_hl.npy', ii_hl)
-    #np.save(npydir + 'rr_v_masses_v_iprs_v_ee_hl.npy',np.vstack((r_sorted_hl,masses_hl,site_iprs_hl,e_sorted_hl)))
-    #np.save(npydir + 'site_state_matrix_hl.npy',site_matrix)
+
+#np.save(npydir + 'ee_hl.npy', ee_hl)
+#np.save(npydir + 'radii_hl.npy', radii_hl)
+#np.save(npydir + 'centers_hl.npy', centers_hl)
+#np.save(npydir + 'ii_hl.npy', ii_hl)
+#np.save(npydir + 'rr_v_masses_v_iprs_v_ee_hl.npy',np.vstack((r_sorted_hl,masses_hl,site_iprs_hl,e_sorted_hl)))
+#np.save(npydir + 'site_state_matrix_hl.npy',site_matrix)
