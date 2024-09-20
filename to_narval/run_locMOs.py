434c7b917ffac12826609a8466ea94e34a321305
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-09-20 19:05:53 -0400

    minor amendments to `run_locMOs.py`

--- to_narval/run_locMOs.py
+++ to_narval/run_locMOs.py
@@ -9,22 +9,13 @@ from rollback_percolate_mac.deploy_percolate import run_locMOs, load_data
 n = int(sys.argv[1])
 struc_type = sys.argv[2]
 mo_type = sys.argv[3]
-psipow = int(sys.argv[4])
 
-if struc_type == '40x40':
-    rmax = 18.03
-elif struc_type == 'tempdot6':
-    rmax = 121.2
-elif struc_type == 'tempdot5':
-    rmax = 198.69
-else:
-    print(f'Structure type {struc_type} is not a valid  entry. Exiting now.')
-    sys.exit()
 
 temps = np.arange(40,440,10)
 dV = 0.0
 
-run_name = f'rmax_{rmax}_psipow{psipow}_locMOs_var_a'
+run_name = f'{mo_type}_loc_var_a'
+output_dir = f'{run_name}_pkls'
 
 pos, e, M, gamL, gamR = load_data(n, struc_type, mo_type, compute_gammas='load')
-run_locMOs(pos,M,gamL,gamR,temps, dV, eF=np.min(e),run_name=run_name,use_idprev=False,rmax=rmax, check_sites=True)
+run_locMOs(pos,e,M,gamL,gamR,temps, dV, eF=np.min(e),var_a=True,run_name=run_name,pkl_dir=output_dir)
