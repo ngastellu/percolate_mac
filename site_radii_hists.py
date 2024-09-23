c0bceafd1a8569241a598aea5e0605d4f10e4533
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-09-23 16:06:57 -0400

    Saved simpler site radii hist plotting script as separate script and restored `site_radii_hists.py`

--- site_radii_hists.py
+++ site_radii_hists.py
@@ -1,40 +1,92 @@
 #!/usr/bin/env python
 
 import numpy as np
-import os
 import matplotlib.pyplot as plt
+import os
 from qcnico.plt_utils import setup_tex, MAC_ensemble_colours
 
 
+
+def gen_radii(ddir,eps,return_isample=False):
+    sites_ddir = f'var_radii_data/to_local_sites_data/' 
+    sampdirs = os.listdir(ddir + sites_ddir)
+    print(sampdirs)
+    # print(sampdirs)
+    for d in sampdirs:
+        # print(d)
+        # print('\n'+d.split('-')[1], end=' ')
+        try:
+            # yield np.load(ddir + sites_ddir + d + f'/sites_data_{eps}/radii.npy')
+            if return_isample:
+                yield np.load(ddir + sites_ddir + d + '/sites_data_0.00105/radii.npy'), int(d.split('-')[-1])
+            else:
+                yield np.load(ddir + sites_ddir + d + '/sites_data_0.00105/radii.npy')
+        except FileNotFoundError as e:
+            print('NPY not found!')
+            if return_isample:
+                yield np.array([0]), int(d.split('-')[-1 ])
+            else:
+                yield np.array([0])
+
+
+def gen_data(ddir,eps):
+    sites_ddir = 'var_radii_data/to_local_sites_data/' 
+    sampdirs = os.listdir(ddir + sites_ddir)
+    # print(sampdirs)
+    for d in sampdirs:
+        # print('\n'+d.split('-')[1], end=' ')
+        try:
+            # yield np.load(ddir + sites_ddir + d + f'/sites_data_{eps}/radii.npy')
+            yield np.load(ddir + sites_ddir + d + f'/sites_data_{eps}/rr_v_masses_v_iprs_v_ee.npy')[2,:]
+        except FileNotFoundError as e:
+            # print('NPY not found!')
+            yield np.array([0])
+
+simdir = '/Users/nico/Desktop/simulation_outputs/'
+percdir = simdir + 'percolation/'
+
+eps = '0.00105'
+
 structypes = ['40x40', 'tempdot6', 'tempdot5']
-lbls = ['sAMC-500', 'sAMC-q400', 'sAMC-300']
+lbls= ['PixelCNN', '$\\tilde{T} = 0.6$', '$\\tilde{T} = 0.5$']
 clrs = MAC_ensemble_colours()
 
-percdir = '/Users/nico/Desktop/simulation_outputs/percolation/'
+bins = np.arange(200)
+# bins = np.linspace(0,11,100)
+centers = (bins[:-1] + bins[1:]) * 0.5
+dx = bins[1] - bins[0]
 
 setup_tex()
 fig, axs = plt.subplots(3,1,sharex=True)
 
-bins = np.linspace(0,82,201)
-
-for st, lbl, ax, c in zip(structypes,lbls,axs,clrs):
-    rdir = percdir + f'{st}/var_radii_data/radii_0.00105_psi_pow2_hi/'
-    radii = np.hstack([np.load(rdir + f) for f in os.listdir(rdir)]) 
-    print('MAX RADIUS = ', np.max(radii))
-    hist, bin_edges = np.histogram(radii, bins)
-    centers = (bin_edges[:-1] + bin_edges[1:]) / 2 
-    
-    dx = centers[1] - centers[0]
-
+for ax, st, lbl, c in zip(axs,structypes,lbls,clrs):
+    ntiny = 0
+    datadir = percdir + st + '/'
+    radii_gen = gen_radii(datadir,eps,return_isample=True)
+    radii0k = next(radii_gen)
+    radii0, k = radii0k
+    # radii_gen = gen_data(datadir,eps)
+    # radii0 = next(radii_gen)
+    # ntiny += (radii0 <= 1).sum()
+    hist, _ = np.histogram(radii0,bins=bins)
+    rmax = np.max(radii0)
+    rmax_ind = np.argmax(radii0)
+    # rmax_isample = k
+    for radiik in radii_gen:
+        radii, k = radiik
+        if np.max(radii) > rmax:
+            rmax = np.max(radii)
+            rmax_ind = np.argmax(radii)
+            # rmax_isample = k
+        hist += np.histogram(radii,bins=bins)[0]
+        ntiny += (radii <= 1).sum()
+    print(f'****** MAX RADII INFO FOR {st} ******')
+    print('\trmax = ', rmax)
+    print('\tState index = ', rmax_ind)
+    # print('\tSample index = ', rmax_isample)
     ax.bar(centers, hist,align='center',width=dx,color=c,label=lbl)
     ax.legend()
-    ax.set_ylabel('Counts (log)')
     ax.set_yscale('log')
     # print(f'{st} ensemble has {ntiny} radii <= 1')
 
-ax.set_xlabel('Site radius $a$ [\AA]')# / \# crystalline atoms in structure')
-
-plt.suptitle('Radii of sites from 100 highest-energy MOs')
-
 plt.show()
-
