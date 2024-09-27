13f8480f66bfc6d46cea85f788d384292252f177
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-09-27 16:07:03 -0400

    Modified `obtain_conudctivities.py` to run on pure (localised) MOs

--- to_narval/obtain_conductivities.py
+++ to_narval/obtain_conductivities.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import numpy as np
-from var_a_percolate_mac.utils_analperc import get_dcrits, sigma_errorbar
+from var_a_percolate_mac.utils_analperc import get_dcrits, load_dcrits, sigma_errorbar
 import os
 import sys
 
 
 
-structype = sys.argv[1]
-motype = sys.argv[2]
+structype = os.path.basename(os.getcwd())
+motype = sys.argv[1]
 psipow = 2
-temps = np.arange(180,440,10)
+# temps = np.arange(180,440,10)
 
 if structype == '40x40':
     rmax = 18.03
@@ -19,14 +19,17 @@ elif structype == 'tempdot5':
     rmax = 198.69
 
 
-run_name = f'rmax_{rmax}_psipow{psipow}_sites_gammas_{motype}'
+#run_name = f'rmax_{rmax}_psipow{psipow}_sites_gammas'
+run_name = f'{motype}_loc_var_a'
 
 datadir = f'{run_name}_symlinks'
-with open(os.path.join(datadir, f'good_runs_rmax_{rmax}_psipow{psipow}_sites_gammas_{motype}.txt')) as fo:
+with open(os.path.join(datadir, f'good_runs_{run_name}.txt')) as fo:
     nn = [int(l.strip()) for l in fo]
 
 
-dcrits = get_dcrits(nn, temps, datadir, pkl_prefix=f'out_percolate_{run_name}')
+# dcrits = get_dcrits(nn, temps, datadir, pkl_prefix=f'out_percolate_{run_name}')
+
+temps, dcrits = load_dcrits(nn, run_name)
 
 sigmas, sigmas_err = sigma_errorbar(dcrits)
 
