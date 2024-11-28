5f4e6a7c424f503c36430c47de222ff6ee932a1b
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-11-28 10:27:04 -0500

    Copied `run_var_from_sites` from `rollback` branch. Renamed `run_var_a_locMOs` to `run_percolate_from_MOs`. Got rid of `run_percolate`.

--- deploy_percolate.py
+++ deploy_percolate.py
@@ -2,15 +2,16 @@
 
 import pickle
 from time import perf_counter
-from os import path
+import os
 import numpy as np
 import qcnico.qchemMAC as qcm
 from qcnico.coords_io import read_xsf, read_xyz
+from qcnico.rotate_pos import rotate_pos
+from qcnico.data_utils import save_npy
 from qcnico.remove_dangling_carbons import remove_dangling_carbons
 from .percolate import diff_arrs, percolate,\
         diff_arrs_w_inds, jitted_percolate, diff_arrs_var_a
 from .MOs2sites import generate_site_list_opt
-
 from .utils_arpackMAC import remove_redundant_eigenpairs
 
 
@@ -19,14 +20,14 @@ def load_data(sample_index, structype, motype,gammas_method='compute',sort_energ
     This function aims to be be common to all percolation runs (gridMOs or not, etc.). """
 
     if structype == '40x40':
-        pos_dir = path.expanduser('~/scratch/clean_bigMAC/40x40/relaxed_structures_no_dangle/')
+        pos_dir = os.path.expanduser('~/scratch/clean_bigMAC/40x40/relaxed_structures_no_dangle/')
         posfile = f'bigMAC-{sample_index}_relaxed_no-dangle.xyz'
 
     else:
-        pos_dir = path.expanduser(f'~/scratch/clean_bigMAC/{structype}/relaxed_structures_no_dangle/')
+        pos_dir = os.path.expanduser(f'~/scratch/clean_bigMAC/{structype}/relaxed_structures_no_dangle/')
         posfile = f'{structype}n{sample_index}_relaxed_no-dangle.xyz'
     
-    arpackdir = path.expanduser(f'~/scratch/ArpackMAC/{structype}')
+    arpackdir = os.path.expanduser(f'~/scratch/ArpackMAC/{structype}')
 
     
     if motype == 'virtual' or motype == 'occupied' or motype == 'virtual_w_HOMO':
@@ -37,8 +38,8 @@ def load_data(sample_index, structype, motype,gammas_method='compute',sort_energ
         mo_file = f'MOs_ARPACK_{motype}_{structype}-{sample_index}.npy'
         energy_file = f'eARPACK_{motype}_{structype}-{sample_index}.npy'
     
-    mo_path = path.join(arpackdir,'MOs',motype,mo_file)
-    energy_path = path.join(arpackdir,'energies',motype,energy_file)
+    mo_path = os.path.join(arpackdir,'MOs',motype,mo_file)
+    energy_path = os.path.join(arpackdir,'energies',motype,energy_file)
 
     energies = np.load(energy_path)
     M =  np.load(mo_path)
@@ -49,7 +50,7 @@ def load_data(sample_index, structype, motype,gammas_method='compute',sort_energ
         energies = energies[eii]
         M = M[:,eii]
     
-    pos_path = path.join(pos_dir,posfile)
+    pos_path = os.path.join(pos_dir,posfile)
     pos = read_xyz(pos_path)
  
     # ******* 2: Get gammas *******
@@ -90,10 +91,10 @@ def load_data(sample_index, structype, motype,gammas_method='compute',sort_energ
 
 
 def load_var_a_data(datadir='.'):
-    sites_pos = np.load(path.join(datadir, 'centers.npy'))
-    sites_energies = np.load(path.join(datadir, 'ee.npy'))
-    sites_radii = np.load(path.join(datadir, 'radii.npy'))
-    ii = np.load(path.join(datadir, 'ii.npy'))
+    sites_pos = np.load(os.path.join(datadir, 'centers.npy'))
+    sites_energies = np.load(os.path.join(datadir, 'ee.npy'))
+    sites_radii = np.load(os.path.join(datadir, 'radii.npy'))
+    ii = np.load(os.path.join(datadir, 'ii.npy'))
 
     return sites_pos, sites_energies, sites_radii, ii
 
@@ -104,13 +105,13 @@ def load_data_multi(sample_index, structype, motypes, e_file_names=None, MO_file
     use more eigenpairs for percolation calculation."""
 
     if structype == 'pCNN':
-            arpackdir = path.expanduser('~/scratch/ArpackMAC/40x40')
-            pos_dir = path.expanduser('~/scratch/clean_bigMAC/40x40/relax/no_PBC/relaxed_structures')
+            arpackdir = os.path.expanduser('~/scratch/ArpackMAC/40x40')
+            pos_dir = os.path.expanduser('~/scratch/clean_bigMAC/40x40/relax/no_PBC/relaxed_structures')
 
             posfile = f'bigMAC-{sample_index}_relaxed.xsf'
     else:
-            arpackdir = path.expanduser(f'~/scratch/ArpackMAC/{structype}')
-            pos_dir = path.expanduser(f'~/scratch/clean_bigMAC/{structype}/sample-{sample_index}/')
+            arpackdir = os.path.expanduser(f'~/scratch/ArpackMAC/{structype}')
+            pos_dir = os.path.expanduser(f'~/scratch/clean_bigMAC/{structype}/sample-{sample_index}/')
 
             posfile = f'{structype}n{sample_index}_relaxed.xsf'
 
@@ -125,15 +126,15 @@ def load_data_multi(sample_index, structype, motypes, e_file_names=None, MO_file
         energy_files = [efn + f'-{sample_index}.npy' for efn in e_file_names]
 
 
-    mo_paths = [path.join(arpackdir,'MOs',motype,mo_file) for (motype, mo_file) in zip(motypes, mo_files)]
-    energy_paths = [path.join(arpackdir,'energies',motype, energy_file) for (motype, energy_file) in zip(motypes, energy_files)]
+    mo_paths = [os.path.join(arpackdir,'MOs',motype,mo_file) for (motype, mo_file) in zip(motypes, mo_files)]
+    energy_paths = [os.path.join(arpackdir,'energies',motype, energy_file) for (motype, energy_file) in zip(motypes, energy_files)]
 
     energies = np.hstack([np.load(energy_path) for energy_path in energy_paths])
     M =  np.hstack([np.load(mo_path) for mo_path in mo_paths])
 
     energies, M = remove_redundant_eigenpairs(energies, M)
     
-    pos_path = path.join(pos_dir,posfile)
+    pos_path = os.path.join(pos_dir,posfile)
     pos, _ = read_xsf(pos_path)  
     
     rCC = 1.8
@@ -178,20 +179,20 @@ def setup_hopping_sites_gridMOs(pos, energies, M, gamL, gamR, tolscal=3.0, nbins
         
         centres, ee, ii = generate_site_list_opt(pos,M,L_mos,R_mos,energies,nbins=100)
         if save_centers:
-            np.save(path.join(datapath,f'cc.npy'),centres)
-            np.save(path.join(datapath,f'ee.npy'),ee)
-            np.save(path.join(datapath,f'ii.npy'), ii)
+            np.save(os.path.join(datapath,f'cc.npy'),centres)
+            np.save(os.path.join(datapath,f'ee.npy'),ee)
+            np.save(os.path.join(datapath,f'ii.npy'), ii)
     else:
         try:
-            centres = np.load(path.join(datapath, 'cc.npy'))
-            ee = np.load(path.join(datapath, 'ee.npy'))
-            ii = np.load(path.join(datapath, 'ii.npy'))
+            centres = np.load(os.path.join(datapath, 'cc.npy'))
+            ee = np.load(os.path.join(datapath, 'ee.npy'))
+            ii = np.load(os.path.join(datapath, 'ii.npy'))
         except FileNotFoundError:
             print('Hopping centre files not found. Recomputing...')
             centres, ee, ii = generate_site_list_opt(pos,M,L_mos,R_mos,energies,nbins=nbins)
-            np.save(path.join(datapath, f'cc.npy',centres))
-            np.save(path.join(datapath, f'ee.npy',ee))
-            np.save(path.join(datapath, f'ii.npy', ii))
+            np.save(os.path.join(datapath, f'cc.npy',centres))
+            np.save(os.path.join(datapath, f'ee.npy',ee))
+            np.save(os.path.join(datapath, f'ii.npy', ii))
     
     cgamL = gamL[ii]
     cgamR = gamR[ii]
@@ -204,58 +205,112 @@ def setup_hopping_sites_gridMOs(pos, energies, M, gamL, gamR, tolscal=3.0, nbins
     else:
         return centres, ee, L_sites, R_sites
 
-def run_percolate(sites_pos, sites_energies, L, R, all_Ts, dV, eF=0, a0=30, pkl_dir='.',jitted=False,pkl_prefix='out_percolate',rmax=None):
-    """Once the hopping sites have been defined, run `percolate` under the array of desired conditions (temperature, external field).
-    This function returns nothing; it writes the results of each call to `percolate` to a pickle file located in `pkl_dir`."""
-    kB = 8.617333262 #eV / K
+
+def run_percolate_from_sites(pos, M, S, all_Ts, dV, tol_scal=3.0 ,eF=0, hyperlocal=False,npydir='./var_a_npys',run_name=None,rmax=None, dE_max=None, sGammas=None,gamma=0.1,pkl_dir='.',rotate=False,dcrits_npy=True):
+    
+    if not os.path.isdir(pkl_dir):
+        os.mkdir(pkl_dir)
+
+    # Ensure site kets are properly normalised
+    S /= np.linalg.norm(S,axis=0)
+
+
+
+    # ******* Pre-compute distances *******
+    if hyperlocal:
+        centres = np.load(os.path.join(npydir,'centers_hl.npy'))
+        ee = np.load(os.path.join(npydir,'ee_hl.npy'))
+        radii = np.load(os.path.join(npydir, 'radii_hl.npy'))
+
+    else:
+        centres = np.load(os.path.join(npydir,'centers.npy'))
+        ee = np.load(os.path.join(npydir,'ee.npy'))
+        radii = np.load(os.path.join(npydir, 'radii.npy'))
+    
     if np.abs(dV) > 0:
-        dX = np.max(sites_pos[:,0]) - np.min(sites_pos[:,0])
+        dX = np.max(pos[:,0]) - np.min(pos[:,0])
         E = np.array([dV/dX,0])
     else:
         E = np.array([0.0,0.0])
     
-    if isinstance(a0, np.ndarray):
-        print('~~~~ a0 is an array: running variable radii percolation ~~~~')
-        if rmax is not None:
-            igood = (a0 < rmax).nonzero()[0] # filter out sites that are 'too big'
-            sites_pos = sites_pos[igood]
-            sites_energies = sites_energies[igood]
-            a0 = a0[igood]
-            
-        edArr, rdArr, ij = diff_arrs_var_a(sites_energies, sites_pos, radii=a0, eF=eF, E=E)
-        var_a = True
+    nsites = ee.shape[0]
+
+    if rmax is not None:
+        rfilter = radii < rmax
     else:
-        print('~~~~ a0 is a scalar: running variable single-radius percolation ~~~~')
-        edArr, rdArr, ij = diff_arrs_w_inds(sites_energies, sites_pos, a0=a0, eF=eF, E=E)
-        var_a = False
+        rfilter = np.ones(nsites,dtype='bool')
+
+    if dE_max is not None:
+        efilter = np.abs(ee - eF) <= dE_max # max allowed distance from Fermi energy
+    else:
+        efilter = np.ones(nsites, dtype='bool')
     
-    k=0
+    igood = (rfilter * efilter).nonzero()[0] # apply both filters, get remaining valid indices
 
-    #all_Ts = np.sort(all_Ts)[::-1] # sort in reverse order to leverage `prev_d_ind` speedup
-    prev_d_ind = 0
-    for T in all_Ts:
-        print(f'******* T = {T} K *******')
-        darr = rdArr + (edArr / (kB * T))
+    centres = centres[igood]
+    ee = ee[igood]
+    radii = radii[igood]
+    S = S[:,igood]
+
+    np.save(os.path.join(pkl_dir, 'igood.npy'), igood)
+    print('saved igood!', flush=True)
+    
+    if rotate:
+        pos = rotate_pos(pos)
+        centres = rotate_pos(centres)
+
+    if sGammas is None:
+        print('Getting AO gamma...', end=' ')
         start = perf_counter()
-        if jitted:
-            conduction_clusters, dcrit, A = jitted_percolate(darr,ij,L,R)
-        else:
-            conduction_clusters, dcrit, A, prev_d_ind = percolate(darr,ij,L,R,return_adjmat=True,prev_d_ind=0)
+        agaL, agaR = qcm.AO_gammas(pos, gamma)
+        ao_end = perf_counter()
+        print(f'Done! [{ao_end - start} seconds]\nGetting sites gammas...', end=' ')
+        cgamL, cgamR = qcm.MO_gammas(S, agaL, agaR, return_diag=True)
         end = perf_counter()
-        if k >0: # don't time first call to percolate (avoid measuring compilation time)
-            print(f'\n~~~Done! [{end-start} seconds]~~~\n Saving to pkl file...~~~',flush=True)
+        print(f'Done! [{end - ao_end} seconds]')
+        save_npy(cgamL, f'gamL_{run_name}.npy', npydir)
+        save_npy(cgamR, f'gamR_{run_name}.npy', npydir)
+        
+    else:
+        cgamL, cgamR = sGammas
 
+    gamL_tol = np.mean(cgamL) + tol_scal * np.std(cgamL)
+    gamR_tol = np.mean(cgamR) + tol_scal * np.std(cgamR)
+    
+    edArr, rdArr = diff_arrs_var_a(ee, centres, radii, eF=eF, E=E)
+    
+    tracker_file = f'finished_temps_{run_name}_sites_var_a.txt'
+    ftrack = open(tracker_file,'w')
+
+    if dcrits_npy:
+        dcrits = np.zeros(all_Ts.shape[0])
+
+    for k, T in enumerate(all_Ts):
+        # ******* 5: Get spanning cluster *******
+        conduction_clusters, dcrit, A, iidprev = percolate(ee, pos, M, T, gamL_tol=gamL_tol,gamR_tol=gamR_tol, return_adjmat=True, distance='logMA',MOgams=(cgamL, cgamR), dArrs=(edArr,rdArr))
+
+        if dcrits_npy:
+            dcrits[k] = dcrit
 
         
-        pkl_name = pkl_prefix +  f'-{T}K.pkl'
+        if run_name is None:
+            if hyperlocal:
+                pkl_name = f'out_var_a_hl_rollback_percolate-{T}K.pkl'
+            else:
+                pkl_name = f'out_var_a_rollback_percolate-{T}K.pkl'
+        else:
+            pkl_name = 'out_percolate_' + run_name + f'-{T}K.pkl'
 
-        with open(path.join(pkl_dir, pkl_name), 'wb') as fo:
+        with open(os.path.join(pkl_dir, pkl_name), 'wb') as fo:
             pickle.dump((conduction_clusters,dcrit,A), fo)
-        print('Saved successfully.\n', flush=True)
-        k+=1
+        ftrack.write(f'{T}K\n')
+    ftrack.close()
+
+    if dcrits_npy:
+        np.save(os.path.join(pkl_dir, f'dcrits.npy'),np.vstack((all_Ts,dcrits)))
 
 
-def run_percolate_locMOs(pos, energies, M,gamL, gamR, all_Ts, eF, dV, tolscal=3.0, pkl_dir='.'):
+def run_percolate_from_MOs(pos, energies, M,gamL, gamR, all_Ts, eF, dV, tolscal=3.0, pkl_dir='.'):
     """This function combines `setup_hopping_sites` and `run_percolate` for the cases where we want to 
     do percolation on sites obtained without gridifying each MO."""
     kB = 8.617333262 #eV / K
@@ -282,6 +337,6 @@ def run_percolate_locMOs(pos, energies, M,gamL, gamR, all_Ts, eF, dV, tolscal=3.
         conduction_clusters, dcrit, A = percolate(darr,L,R,return_adjmat=True)
         print('\nDone! Saving to pkl file...')
 
-        with open(path.join(pkl_dir, f'out_percolate-{T}K.pkl'), 'wb') as fo:
+        with open(os.path.join(pkl_dir, f'out_percolate-{T}K.pkl'), 'wb') as fo:
             pickle.dump((conduction_clusters,dcrit,A), fo)
         print('Saved successfully.\n')
